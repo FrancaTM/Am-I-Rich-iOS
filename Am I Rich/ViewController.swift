a4af40ed7d3a701c61c78e9ad2512a8897c24de8
FrancaTM <francatm@outlook.com>
2020-05-30 00:31:12 -0300

    Initial Commit

---
+++ Am I Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Am I Rich
+//
+//  Created by Tulio Marcos Franca on 30/05/20.
+//  Copyright © 2020 francaTM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
